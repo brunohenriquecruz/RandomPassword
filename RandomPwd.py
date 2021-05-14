060f02af6f984b3c537d2c07df8dfd23b2e2a431
Bruno Henrique da Cruz <brunohenriquecruz@yahoo.com.br>
2021-05-14 17:19:11 -0300

    Testing random password with inputs

--- RandomPwd.py
+++ RandomPwd.py
@@ -2,16 +2,26 @@
 
 import random
 
-lower   = "abcdefghijklmnopqrstyvwxyz"
+#lower   = "abcdefghijklmnopqrstyvwxyz"
 upper   = "ABCDEFGHIJKLMNOPQRSTYVWXYZ"
 numbers  = "0123456789"
 symbols = "*!@#$%&_"
 
-all = lower + upper + numbers + symbols
+#all = lower + upper + numbers + symbols
 
-length   = 16
-password = "".join(random.sample(all,length))
-print(password)
+lower = input("Digite os caracteres de A - Z: ")
+symbols = input("Digite os símbolos desejados no password: ")
+length = input("Digite o tamanho do password desejado de 1 - 10: ")
+
+all = lower + symbols
+
+
+random = "".join(random.sample(all, int(length)))
+print("Este é o seu password: %s" %random)
+
+#length   = 16
+#password = "".join(random.sample(all,length))
+#print(password)
 
 
 
